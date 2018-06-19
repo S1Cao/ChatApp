9955eeb36461ebf2a8c2a7f077fd971288fd8b67
6093 <6093@ait-sydney.net.au>
2018-06-19 20:43:25 +1000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,40 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule} from 'angularfire2';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ChatPage} from '../pages/chat/chat';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+
+var config = {
+    apiKey: "AIzaSyDa-mAf0dUVxY-B3DtDn8WD--YRTjdngiQ",
+    authDomain: "realtimechat-c19aa.firebaseapp.com",
+    databaseURL: "https://realtimechat-c19aa.firebaseio.com",
+    projectId: "realtimechat-c19aa",
+    storageBucket: "realtimechat-c19aa.appspot.com",
+    messagingSenderId: "981520778328"
+  };
+  
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+	ChatPage,
+	
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+	AngularFireModule.initializeApp(config),
+	AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+	ChatPage
   ],
   providers: [
     StatusBar,
